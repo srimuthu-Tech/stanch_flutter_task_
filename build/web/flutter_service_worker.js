d59aaacaabe59beab6b6e2e2b4492bce1b950f65
srimuthu <srimuthualagu@gmail.com>
2024-10-10 23:32:21 +0530

    Deploy Flutter web app to GitHub Pages

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -32,7 +32,7 @@ const RESOURCES = {"assets/asset/facebook_icon.png": "582d2b8ee60f61845d03e7b6c7
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "d24f4fa9ce283beb2f321d4750a08517",
+"flutter_bootstrap.js": "d32690c0e83321e29aa86b5acb6c6113",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
